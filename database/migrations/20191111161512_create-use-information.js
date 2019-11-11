2e9e737b1197126a91667e2ac2ab1a327e099c2d
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-11 18:33:08 +0100

    made a correction

--- database/migrations/20191111161512_create-use-information.js
+++ database/migrations/20191111161512_create-use-information.js
@@ -10,7 +10,7 @@ exports.up = function(knex) {
     });
   };
   
-  exports.down = function(knex, Promise) {
+  exports.down = function(knex) {
     return knex.schema.dropTableIfExists('users');
   };
   
