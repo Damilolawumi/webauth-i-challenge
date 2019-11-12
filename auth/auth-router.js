9291964626b5d4d8616399926ae461be6da7be22
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-12 18:57:44 +0100

    made a few corrections

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -38,19 +38,14 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
-                // if creds good, stuff a thing inside req.session
-                // which will be there when other endpoints are hit
-                // by the same client
                 req.session.user = user;
-                res.status(200).json({
-                    message: `Welcome ${user.username}!`,
-                });
+                res.status(200).json({ message: `Welcome ${user.username}!` });
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' });
             }
         })
         .catch(error => {
-            res.status(500).json(error);
+            res.status(500).json(error.message);
         });
 });
 
